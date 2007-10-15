51caf9f8919f495cc4bd2c8ed88a4482b89e8919
Tim van der Leeuw <tim.van.der.leeuw@ibissource.org>
2007-10-15 11:35:30 +0000

    Use ClassUtil to open file as Stream

--- JavaSource/nl/nn/adapterframework/util/AbstractXPathUtil.java
+++ JavaSource/nl/nn/adapterframework/util/AbstractXPathUtil.java
@@ -9,6 +9,7 @@
 
 package nl.nn.adapterframework.util;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -26,12 +27,51 @@ import nl.nn.adapterframework.core.IbisException;
  */
 abstract public class AbstractXPathUtil implements XPathUtil {
 
+    /* (non-Javadoc)
+     * @see nl.nn.adapterframework.util.XPathUtil#parseXpath(java.lang.String, java.lang.String)
+     */
     public List parseXpath(String xpathExpression, String resourceName) throws IbisException {
-        return parseXpath(xpathExpression, this.getClass().getResourceAsStream(resourceName));
+        InputStream in = null;
+        try {
+            in = ClassUtils.getResourceURL(this, resourceName).openStream();
+            return parseXpath(xpathExpression, in);
+        } catch (IOException ex) {
+            throw new IbisException(ex);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 
+    /* (non-Javadoc)
+     * @see nl.nn.adapterframework.util.XPathUtil#parseXpathToString(java.lang.String, java.io.InputStream)
+     */
     public String parseXpathToString(String xpathExpression, InputStream in) throws IbisException {
         List result = parseXpath(xpathExpression, in);
+        return listToString(result);
+    }
+
+    /* (non-Javadoc)
+     * @see nl.nn.adapterframework.util.XPathUtil#parseXpathToString(java.lang.String, java.lang.String)
+     */
+    public String parseXpathToString(String xpathExpression, String resourceName) throws IbisException {
+        List result  = parseXpath(xpathExpression, resourceName);
+        return listToString(result);
+    }
+
+    /**
+     * Convert a list to a String
+     * 
+     * Appends all items from the list without any spaces or breaks
+     * @param result
+     * @return
+     */
+    protected String listToString(List result) {
         StringBuffer b = new StringBuffer();
         for (Iterator it = result.iterator(); it.hasNext();) {
             String item = (String) it.next();
@@ -40,10 +80,12 @@ abstract public class AbstractXPathUtil implements XPathUtil {
         return b.toString();
     }
 
-    public String parseXpathToString(String xpathExpression, String resourceName) throws IbisException {
-        return parseXpathToString(xpathExpression, this.getClass().getResourceAsStream(resourceName));
-    }
-
+    /**
+     * Make a list of String from a DOM NodeList contains Node elements.
+     * @param nodes
+     * @return
+     * @throws DOMException
+     */
     protected List makeListFromNodeList(NodeList nodes) throws DOMException {
         List result = new ArrayList(nodes.getLength());
         for (int i = 0; i < nodes.getLength(); ++i) {
