1ae82cb7c7f5b782fd62d035473216857e2f924e
Tim van der Leeuw <tim.van.der.leeuw@ibissource.org>
2007-11-15 09:54:23 +0000

    * Add more detailed logging * Do not attempt to start/stop a receiver which is in state 'Starting'

--- JavaSource/nl/nn/adapterframework/ejb/ListenerPortPoller.java
+++ JavaSource/nl/nn/adapterframework/ejb/ListenerPortPoller.java
@@ -2,7 +2,11 @@
  * ListenerPortPoller.java
  *  
  * $Log: ListenerPortPoller.java,v $
- * Revision 1.1.2.3  2007-11-06 09:39:13  europe\M00035F
+ * Revision 1.1.2.4  2007-11-15 09:54:23  europe\M00035F
+ * * Add more detailed logging
+ * * Do not attempt to start/stop a receiver which is in state 'Starting'
+ *
+ * Revision 1.1.2.3  2007/11/06 09:39:13  Tim van der Leeuw <tim.van.der.leeuw@ibissource.org>
  * Merge refactoring/renaming from HEAD
  *
  * Revision 1.1.2.2  2007/10/25 08:36:57  Tim van der Leeuw <tim.van.der.leeuw@ibissource.org>
@@ -25,11 +29,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.core.IPortConnectedListener;
 import nl.nn.adapterframework.core.ListenerException;
 import nl.nn.adapterframework.receivers.GenericReceiver;
 import nl.nn.adapterframework.util.LogUtil;
 import nl.nn.adapterframework.util.RunStateEnum;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.DisposableBean;
 
 /**
  * The ListenerPortPoller checks for all registered EjbListenerPortConnector
@@ -44,10 +50,15 @@ import org.apache.log4j.Logger;
  * @author Tim van der Leeuw
  * @version Id
  */
-public class ListenerPortPoller {
+public class ListenerPortPoller implements DisposableBean {
     private Logger log = LogUtil.getLogger(this);
     
-    private List jmsConfiguratorList = new ArrayList();
+    private List portConnectorList = new ArrayList();
+
+    public ListenerPortPoller() {
+        log.debug("Created new ListenerPortPoller: " + this.toString());
+    }
+    
     
     /**
      * Add an EjbListenerPortConnector instance to be polled.
@@ -56,7 +67,7 @@ public class ListenerPortPoller {
      */
     public void registerEjbListenerPortConnector(EjbListenerPortConnector ejc) {
         if (!isRegistered(ejc)) {
-            jmsConfiguratorList.add(new WeakReference(ejc));
+            portConnectorList.add(new WeakReference(ejc));
         }
     }
     
@@ -64,7 +75,7 @@ public class ListenerPortPoller {
      * Remove an EjbListenerPortConnector instance from the list to be polled.
      */
     public void unregisterEjbListenerPortConnector(EjbListenerPortConnector ejc) {
-        for (Iterator iter = jmsConfiguratorList.iterator(); iter.hasNext();) {
+        for (Iterator iter = portConnectorList.iterator(); iter.hasNext();) {
             WeakReference wr = (WeakReference)iter.next();
             Object referent = wr.get();
             if (referent == null || referent == ejc) {
@@ -74,7 +85,7 @@ public class ListenerPortPoller {
     }
     
     public boolean isRegistered(EjbListenerPortConnector ejc) {
-        for (Iterator iter = jmsConfiguratorList.iterator(); iter.hasNext();) {
+        for (Iterator iter = portConnectorList.iterator(); iter.hasNext();) {
             WeakReference wr = (WeakReference)iter.next();
             Object referent = wr.get();
             if (referent == null) {
@@ -89,7 +100,7 @@ public class ListenerPortPoller {
      * Unregister all registered EjbListenerPortConnector instances in one go.
      */
     public void clear() {
-        jmsConfiguratorList.clear();
+        portConnectorList.clear();
     }
     /**
      * Poll all registered EjbListenerPortConnector instances to see if they
@@ -97,46 +108,78 @@ public class ListenerPortPoller {
      * toggle their state if not.
      */
     public void poll() {
-        for (Iterator iter = jmsConfiguratorList.iterator(); iter.hasNext();) {
+        if (log.isDebugEnabled()) {
+            log.debug("Enter polling " + this.toString() + ", thread: " + Thread.currentThread().getName());
+        }
+        for (Iterator iter = portConnectorList.iterator(); iter.hasNext();) {
             WeakReference wr = (WeakReference)iter.next();
-            EjbListenerPortConnector EjbListenerPortConnector = (EjbListenerPortConnector) wr.get();
-            if (EjbListenerPortConnector == null) {
+            EjbListenerPortConnector elpc = (EjbListenerPortConnector) wr.get();
+            if (elpc == null) {
                 iter.remove();
                 continue;
             }
+            // Check for each ListenerPort if it's state matches with the
+            // state that IBIS thinks it should be in.
+            IPortConnectedListener listener = elpc.getJmsListener();
             try {
-                if (EjbListenerPortConnector.isClosed() != EjbListenerPortConnector.isListenerPortClosed()) {
-                    toggleConfiguratorState(EjbListenerPortConnector);
+                if (elpc.isClosed() != elpc.isListenerPortClosed()) {
+                    log.info("State of listener [" + listener.getName()
+                            + "] does not match state of WebSphere ListenerPort ["
+                            + elpc.getListenerPortName(listener)
+                            + "] to which it is attached; will try to change state of Receiver ["
+                            + listener.getReceiver().getName() +"]");
+                    toggleConfiguratorState(elpc);
                 }
             } catch (Exception ex) {
                 log.error("Cannot change, or enquire on, state of Listener ["
-                        + EjbListenerPortConnector.getJmsListener().getName() + "]", ex);
+                        + listener.getName() + "]", ex);
             }
         }
 
+        if (log.isDebugEnabled()) {
+            log.debug("Exit polling " + this.toString() + ", thread: " + Thread.currentThread().getName());
+        }
     }
 
     /**
      * Toggle the state of the EjbListenerPortConnector instance by starting/stopping
      * the receiver it is attached to (via the JmsListener).
+     * This method changes the state of the Receiver to match the state of the
+     * WebSphere ListenerPort.
      */
-    public void toggleConfiguratorState(EjbListenerPortConnector EjbListenerPortConnector) throws ConfigurationException {
-        GenericReceiver receiver = (GenericReceiver) EjbListenerPortConnector.getJmsListener().getReceiver();
-        if (EjbListenerPortConnector.isListenerPortClosed()) {
-            log.info("Stopping Receiver [" + receiver.getName() + "] because the WebSphere Listener-Port is in state 'stopped' but the JmsConnector in state 'open'");
-            receiver.stopRunning();
+    public void toggleConfiguratorState(EjbListenerPortConnector elpc) throws ConfigurationException {
+        GenericReceiver receiver = (GenericReceiver) elpc.getJmsListener().getReceiver();
+        if (elpc.isListenerPortClosed()) {
+            if (receiver.isInRunState(RunStateEnum.STARTED)) {
+                log.info("Stopping Receiver [" + receiver.getName() + "] because the WebSphere Listener-Port is in state 'stopped' but the JmsConnector in state 'open' and the receiver is in state 'started'");
+                receiver.stopRunning();
+            } else {
+                log.info("ListenerPort [" + elpc.getListenerPortName(elpc.getJmsListener())
+                        + "] is in closed state, Listener is not in closed state, but Receiver is in state ["
+                        + receiver.getRunState().getName() + "] so the state of Receiver will not be changed.");
+                
+            }
         } else {
+            // Only start the receiver for adapters which are running.
             if (receiver.getAdapter().getRunState().equals(RunStateEnum.STARTED)) {
-                log.info("Starting Receiver [" + receiver.getName() + "] because the WebSphere Listener-Port is in state 'started' but the JmsConnector in state 'closed'");
-                receiver.startRunning();
+                if (!receiver.isInRunState(RunStateEnum.STARTING)) {
+                    log.info("Starting Receiver [" + receiver.getName() + "] because the WebSphere Listener-Port is in state 'started' but the JmsConnector in state 'closed'");
+                    receiver.startRunning();
+                } else {
+                    log.info("Receiver [" + receiver.getName() + "] still starting, so not changing anything now");
+                }
             } else {
                 try {
                     log.warn("JmsConnector is closed, Adapter is not in state '" + RunStateEnum.STARTED + "', but WebSphere Jms Listener Port is in state 'started'. Stopping the listener port.");
-                    EjbListenerPortConnector.stop();
+                    elpc.stop();
                 } catch (ListenerException ex) {
                     log.error(ex,ex);
                 }
             }
         }
+    }
+
+    public void destroy() throws Exception {
+        clear();
     }
 }
